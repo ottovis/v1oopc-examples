db9d4b9cd7d09d912ed0a80c1fd4261163063174
Wouter van Ooijen <wouter@voti.nl>
2018-06-15 15:47:06 +0200

    update

---
+++ 20-02-adc/main.cpp
@@ -0,0 +1,20 @@
+#include "hwlib.hpp"
+
+int main( void ){	
+    
+   // kill the watchdog
+   WDT->WDT_MR = WDT_MR_WDDIS;
+   
+   // wait for the PC to start the console
+   hwlib::wait_ms( 500 );
+   
+   hwlib::cout << "ADC a0 demo\n";
+   
+   auto pin = hwlib::target::pin_adc( hwlib::target::ad_pins::a0 );
+   
+   for(;;){
+      hwlib::cout << pin.get() << "\n";
+      hwlib::wait_ms( 200 );
+   }
+   
+}
