f730e09fa78f0a5f3007dfa49845b70ec49d734a
Wouter van Ooijen <wouter@voti.nl>
2019-04-01 07:56:51 +0200

    update

---
+++ 00-01-line/line_print.hpp
@@ -0,0 +1,6 @@
+#include "hwlib.hpp"
+
+void line_print( 
+   hwlib::window & w, 
+   int start_x, int start_y, 
+   int end_x, int end_y );
--- 04-01-xyxy/main.cpp
+++ 04-01-xyxy/main.cpp
@@ -1,8 +1,12 @@
 // example:
 // print a line by passing the four coordinates
 
+#linclude "hwlib.hpp"
+
 void line_print( int start_x, int start_y, int end_x, int end_y );
 
 int main(int argc, char **argv){
+   hwlib::target::screen( xy( ))
+   
    line_print( 10, 10, 20, 20 );
 }
